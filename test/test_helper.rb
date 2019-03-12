583af7b6c01ef97c87e2f079cc044f5e10fdf4b1
Eyal Shalev <eyalsh@gmail.com>
2019-03-12 14:26:26 +0200

    Copied test helper from graphite-api

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,41 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require 'simplecov'
+require 'simplecov-rcov'
+require 'codecov'
+require "minitest/autorun"
+
+SimpleCov.start { add_filter "/tests/" }
+SimpleCov.formatter = Class.new do
+  def format(result)
+     SimpleCov::Formatter::Codecov.new.format(result) if ENV["CODECOV_TOKEN"]
+     SimpleCov::Formatter::RcovFormatter.new.format(result) unless ENV["CI"]
+  end
+end
+
 require "graphite-api/middleware"
 
-require "minitest/autorun"
+module GraphiteAPI
+  module Functional
+    class TestCase < Minitest::Test
+      def random_non_repeating_port
+        @ports ||= (1000..9999).to_a.shuffle
+        @ports.pop
+      end
+
+      def stop_em_if_running
+        EM.stop if EM.reactor_running?
+        sleep 0.1 while EM.reactor_running?
+      end
+    end
+  end
+
+  module MockServer
+    def initialize db
+      @db = db
+    end
+    def receive_data data
+      @db.push data
+    end
+  end
+end
